11174198a26bd8ca3edf29d08c7a760929d253e0
Jaden Lund <lundjaden@gmail.com>
2022-07-20 17:54:50 -0500

    commit tree 1734f44d2269622fc9d31190c761e8dc45a68df4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(newCat) {
+    cats.push(newCat);
+    return cats;
+}
+
+
+function destructivelyPrependCat(newCat) {
+    cats.unshift(newCat);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(0);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(cats.length - 1)
+    return cats;
+}
+function appendCat(newCat) {
+    return [...cats, newCat]
+   
+}
+
+
+function prependCat(newCat) {
+   return [newCat,...cats]
+}
+
+function removeLastCat() {
+    const slicedCats = cats.slice(0,cats.length - 1);
+    return slicedCats;
+}
+
+function removeFirstCat() {
+    const slicedCats = cats.slice(1);
+    return slicedCats;
+}
